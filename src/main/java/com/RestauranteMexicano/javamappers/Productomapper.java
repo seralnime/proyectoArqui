53342733b161210628b288f7de0cf314c0f445fe
seralnime <140459650+seralnime@users.noreply.github.com>
2024-05-19 22:09:46 -0500

    ya front_sincambiar

--- src/main/java/com/RestauranteMexicano/javamappers/Productomapper.java
+++ src/main/java/com/RestauranteMexicano/javamappers/Productomapper.java
@@ -1,26 +1,27 @@
-package com.RestauranteMexicano.JavaMappers;
+package com.RestauranteMexicano.javamappers;
 
 import java.util.List;
 import com.RestauranteMexicano.modelos.Producto;
 //import org.apache.ibatis.annotations.Param;
 
-
 /**
-    Diseño y arquitectura de software
-    @author
-        Santiago Sánchez Cárdenas - 0000271976
-        Sergio Gabriel Nieto Meneses - 0000246107
-        Mauricio Andres Valderrama Acosta - 0000251802
-
+ * Diseño y arquitectura de software
+ * 
+ * @author
+ *         Santiago Sánchez Cárdenas - 0000271976
+ *         Sergio Gabriel Nieto Meneses - 0000246107
+ *         Mauricio Andres Valderrama Acosta - 0000251802
+ * 
  */
 public interface ProductoMapper {
 
     public List<Producto> consultarProductos();
 
-    public Producto consultarProducto(int id); 
+    public Producto consultarProducto(int id);
 
     public void insertarProducto(Producto pr);
 
-   // public void restarStock(@Param("idProducto") int idProducto, @Param("cantidadComprada") int cantidadComprada);
-    
+    // public void restarStock(@Param("idProducto") int idProducto,
+    // @Param("cantidadComprada") int cantidadComprada);
+
 }
